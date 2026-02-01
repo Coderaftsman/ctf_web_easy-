345dba256860c019dc4cf4897bd50cdfac53a1db
Coderaftsman <155223503+Coderaftsman@users.noreply.github.com>
2026-02-01 21:49:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+FLAG = "FLAG{inspect_before_trust}"
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    message = ""
+    if request.method == "POST":
+        if request.form.get("flag") == FLAG:
+            return render_template("success.html")
+        else:
+            message = "❌ Wrong flag"
+
+    return render_template("index.html", message=message)
