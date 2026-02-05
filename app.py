620bdac9fc13cf76d2e371ca5319a9730b124bf0
Coderaftsman <155223503+Coderaftsman@users.noreply.github.com>
2026-02-05 22:39:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
-# Store flag securely (can move to env later)
 FLAG = "FLAG{inspect_before_trust}"
 
 
@@ -21,6 +21,7 @@ def home():
     return render_template("index.html", message=message)
 
 
-# IMPORTANT:
-# Do NOT run app.run() for cloud platforms.
-# Gunicorn will automatically detect `app` object.
+# 🔹 LOCALHOST SUPPORT
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # cloud gives PORT, local uses 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
