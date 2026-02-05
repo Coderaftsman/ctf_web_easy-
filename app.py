17a31da6f244caebcd24e5c481836c474eae2561
Coderaftsman <155223503+Coderaftsman@users.noreply.github.com>
2026-02-05 22:00:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 FLAG = "FLAG{inspect_before_trust}"
 
 @app.route("/", methods=["GET", "POST"])
@@ -12,5 +11,6 @@ def home():
             return render_template("success.html")
         else:
             message = "❌ Wrong flag"
-
     return render_template("index.html", message=message)
+
+# No if __name__ == "__main__" block needed for PaaS
